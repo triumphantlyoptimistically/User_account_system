2b6b4958b61a54bc75711a36366c836852af8950
Dylan Speakman <24DS0790@barton.ac.uk>
2026-01-13 14:18:28 +0000

    Add main menu and program flow

--- User_account_system.py
+++ User_account_system.py
@@ -1,1 +1,27 @@
+def Display_Menu():
+    print("User account System\n\n1. Register\n2. Login\n3. Exit\n\n")
 
+def Get_Option():
+    valid = False
+    while valid == False:
+        option = int(input("Enter an option: "))
+        if option < 1 or option > 3:
+            print ("Enter an option between 1 and 3\n")
+        else:
+            valid = True
+            return option
+
+
+
+
+
+running = True
+while running:
+    Display_Menu()
+    option = Get_Option()
+    if option == 1:
+        print("Register selected\n\n")
+    elif option == 2:
+        print("Login selected\n\n")
+    else:
+        running = False
