94f40c31c163cc017dbfc07e577248543bdc4037
Dylan Speakman <24DS0790@barton.ac.uk>
2026-01-13 14:46:49 +0000

    Added .gitignore

--- User_account_system.py
+++ User_account_system.py
@@ -11,16 +11,38 @@ def Get_Option():
             valid = True
             return option
 
-
-
-
+def Register_User():
+    empty = True
+    while empty:
+        username = input("\nEnter username: ")
+        password = input("Enter password: ")
+        if not username and not password:
+            print("\nPlease enter a valid username and passsword")
+        elif not password:
+            print("\nPlease enter a valid password")
+        elif not username:
+            print("\nPlease enter a valid username")
+        else:
+            empty = False
+    with open("Users.txt", "a+") as text_file:
+        text_file.seek(0)
+        for line in text_file:
+            line = line.strip()
+            if not line:
+                continue
+            existingUsername = line.Split(" ", 1)[0]
+            if existingUsername == username:
+                print("\nThat username already exists. Choose another.\n")
+                return 
+        text_file.write(f"{username} {password}\n")
+    print("User registered")
 
 running = True
 while running:
     Display_Menu()
     option = Get_Option()
     if option == 1:
-        print("Register selected\n\n")
+        Register_User()
     elif option == 2:
         print("Login selected\n\n")
     else:
